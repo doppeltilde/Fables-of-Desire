11134acc59f014103c34124e11734f6aaa83d05f
Jona Tadashi <jona@keemail.me>
2021-04-19 10:59:14 +0200

    migration

--- android/app/src/main/java/space/smalldreams/game/beginagain/MainActivity.java
+++
@@ -1,6 +0,0 @@
-package space.smalldreams.game.beginagain;
-
-import io.flutter.embedding.android.FlutterActivity;
-
-public class MainActivity extends FlutterActivity {
-}
--- lib/screens/pages/more/story.dart
+++
@@ -1,9 +0,0 @@
-class Story {
-  String _name, _email;
-  int _age;
-  Story(this._name, this._email, this._age);
-
-  get name => this._name;
-  get email => this._email;
-  get age => this._age;
-}
