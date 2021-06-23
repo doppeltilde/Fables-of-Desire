999839ed3fb668198cd93ec888926a01592cbd02
JONA T. FEUCHT <jona@keemail.me>
2021-06-23 14:58:24 +0200

    fade in/out

--- lib/constructor/vn_scaffold.dart
+++ lib/constructor/vn_scaffold.dart
@@ -16,33 +16,54 @@ class VNScaffold extends StatefulWidget {
 
 class _VNState extends State<VNScaffold> {
   bool isFinished = false;
+  var fadeIn = false;
   @override
   Widget build(BuildContext context) {
     return WillPopScope(
       onWillPop: () => getOnWillPop(),
-      child: Scaffold(
-        resizeToAvoidBottomInset: false,
-        backgroundColor: Colors.black,
-        body: GestureDetector(
-          onTap: () {
-            setState(() {
-              if (widget.textSound.isFinished() == true) {
-                Navigator.of(context).pushNamed(widget.nextRoute);
-              }
-              widget.textSound.nextQuestion();
-            });
-          },
-          child: InterludeTextSound(
-            "assets/images/bgs/" + widget.bgImage + ".jpg",
-            widget.textSound.getCharacterName(),
-            widget.textSound.getCharacterText(),
-            widget.textSound.getNumber(),
-            widget.textSound.getMCImage(),
-            widget.textSound.getCharImage(),
-            widget.route,
-            widget.nextRoute,
-          ),
-        ),
+      child: Builder(
+        builder: (context) {
+          if (fadeIn != false) {
+            return AnimatedOpacity(
+              // If the widget is visible, animate to 0.0 (invisible).
+              // If the widget is hidden, animate to 1.0 (fully visible).
+              opacity: fadeIn ? 1.0 : 0.0,
+              duration: Duration(milliseconds: 500),
+              // The green box must be a child of the AnimatedOpacity widget.
+              child: Container(
+                width: double.infinity,
+                height: double.infinity,
+                color: Colors.black,
+              ),
+            );
+          } else {
+            return Scaffold(
+              resizeToAvoidBottomInset: false,
+              backgroundColor: Colors.black,
+              body: GestureDetector(
+                onTap: () {
+                  setState(() {
+                    if (widget.textSound.isFinished() == true) {
+                      Navigator.of(context).pushNamed(widget.nextRoute);
+                      fadeIn = true;
+                    }
+                    widget.textSound.nextQuestion();
+                  });
+                },
+                child: InterludeTextSound(
+                  "assets/images/bgs/" + widget.bgImage + ".jpg",
+                  widget.textSound.getCharacterName(),
+                  widget.textSound.getCharacterText(),
+                  widget.textSound.getNumber(),
+                  widget.textSound.getMCImage(),
+                  widget.textSound.getCharImage(),
+                  widget.route,
+                  widget.nextRoute,
+                ),
+              ),
+            );
+          }
+        },
       ),
     );
   }
